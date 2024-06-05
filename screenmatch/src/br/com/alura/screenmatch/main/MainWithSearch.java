8fbc82440093651cc1f74c5c2d45a790482d1757
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-05 20:02:57 -0300

    01_09: add code for the HTTP request

--- screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
+++ screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -1,7 +1,27 @@
 package br.com.alura.screenmatch.main;
 
-public class MainWithSearch {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
+public class MainWithSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which movie do you want to search?: ");
+        var query = scanner.next();
+
+        String queryURL = "http://www.omdbapi.com/?t=" + query + "&apikey=337e4e55";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(queryURL))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
