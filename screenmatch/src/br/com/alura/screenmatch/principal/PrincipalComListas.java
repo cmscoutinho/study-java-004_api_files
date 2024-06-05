e0b1535dcf54236a7ca2150387c5e6410871c760
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-05 17:10:08 -0300

    01_09: add code from previous project

---
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,52 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.Filme;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        List<Titulo> assistidos = new ArrayList<>();
+        assistidos.add(filmeDoPaulo);
+        assistidos.add(meuFilme);
+        assistidos.add(outroFilme);
+        assistidos.add(lost);
+
+        for (Titulo item: assistidos) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+        }
+
+        List<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
+
+        System.out.println("Lista de títulos ordenados:");
+        Collections.sort(assistidos);
+        System.out.println(assistidos);
+        assistidos.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordcenando por ano: ");
+        System.out.println(assistidos);
+    }
+}
