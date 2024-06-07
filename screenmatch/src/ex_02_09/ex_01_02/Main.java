8c1e334ef1ab03b43431a155e094a0c25773084a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-06 21:02:53 -0300

    02_09: add code for exercise 2

--- screenmatch/src/ex_02_09/ex_01_02/Main.java
+++ screenmatch/src/ex_02_09/ex_01_02/Main.java
@@ -1,19 +1,23 @@
 package ex_02_09.ex_01_02;
 
 import com.google.gson.Gson;
-import com.google.gson.internal.bind.util.ISO8601Utils;
+import com.google.gson.GsonBuilder;
+
+record Person(String name, int age, String city) {};
 
 public class Main {
     public static void main(String[] args) {
         String json = """
                 {
-                    "nome": "Claudio",
-                    "idade": 35,
-                    "cidade": "Belém"
+                    "name": "Claudio",
+                    "age": 35,
+                    "city": Belém
                 }""";
 
-        Gson gson = new Gson();
-        Pessoa pessoa = gson.fromJson(json, Pessoa.class);
-        System.out.println(pessoa);
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        Person person = gson.fromJson(json, Person.class);
+        System.out.println(person);
     }
 }
--- screenmatch/src/ex_02_09/ex_01_02/Pessoa.java
+++
@@ -1,3 +0,0 @@
-package ex_02_09.ex_01_02;
-
-public record Pessoa(String nome, int idade, String cidade) {}
