cbec88e013df9848d0c549acbbd05072845f7fc3
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-08 10:48:43 -0300

    03_04: add exception handling for illegal arguments

--- screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
+++ screenmatch/src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -20,29 +20,32 @@ public class MainWithSearch {
         var query = scanner.next();
 
         String queryURL = "http://www.omdbapi.com/?t=" + query + "&apikey=337e4e55";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(queryURL))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-//        Title myTitle = gson.fromJson(json, Title.class);
-        TitleOmdb myOmdbTitle = gson.fromJson(json, TitleOmdb.class);
-        System.out.println(myOmdbTitle);
         try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(queryURL))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+//        Title myTitle = gson.fromJson(json, Title.class);
+            TitleOmdb myOmdbTitle = gson.fromJson(json, TitleOmdb.class);
+            System.out.println(myOmdbTitle);
+//        try {
             Title myTitle = new Title(myOmdbTitle);
             System.out.println(myTitle);
             System.out.println("Title converted!");
         } catch (NumberFormatException e) {
             System.out.println("Error: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error! Please check the URI.");
+            System.out.println("Detailed error: " + e.getMessage());
         }
 
         System.out.println("The program ended succesfully!");
