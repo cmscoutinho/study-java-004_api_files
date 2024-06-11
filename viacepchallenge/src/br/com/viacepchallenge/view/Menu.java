290479ab2eb782d282b912260b022bf89616a0ba
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-11 13:01:08 -0300

    create the Menu class

---
+++ viacepchallenge/src/br/com/viacepchallenge/view/Menu.java
@@ -0,0 +1,22 @@
+package br.com.viacepchallenge.view;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void welcome() {
+        System.out.println("Welcome to the ViaCEP API interface!");
+    }
+
+    public String read() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Which CEP (ZIP) code do you want to search? (\"0\" to exit): ");
+        String query = scanner.nextLine();
+        if (query.equals("0")) {
+            return null;
+        } else {
+            return query;
+        }
+    }
+}
