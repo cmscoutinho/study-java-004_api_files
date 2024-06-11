46ee3c48e00975043728fe2999e23124fe63fd27
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-10 23:01:01 -0300

    define toString() of Address

--- viacepchallenge/src/br/com/viacepchallenge/model/Address.java
+++ viacepchallenge/src/br/com/viacepchallenge/model/Address.java
@@ -1,4 +1,13 @@
 package br.com.viacepchallenge.model;
 
 public record Address(String logradouro, String bairro, String localidade, String uf, String cep) {
+    @Override
+    public String toString() {
+        return """
+                Street: %s
+                District: %s
+                City: %s
+                State: %s
+                ZIP Code: %s""".formatted(logradouro, bairro, localidade, uf, cep);
+    }
 }
