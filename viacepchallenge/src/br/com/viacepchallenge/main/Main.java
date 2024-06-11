64358e92841f5da93faa0bb2f870d9df6336ee88
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-10 23:05:25 -0300

    use Address object to display info from JSON

--- viacepchallenge/src/br/com/viacepchallenge/main/Main.java
+++ viacepchallenge/src/br/com/viacepchallenge/main/Main.java
@@ -1,6 +1,9 @@
 package br.com.viacepchallenge.main;
 
 import br.com.viacepchallenge.controller.HttpConnection;
+import br.com.viacepchallenge.model.Address;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 
@@ -18,6 +21,10 @@ public class Main {
             System.out.println("Connection to the remote host failed! Error: " + e.getMessage());
         }
 
-        System.out.println(connection.getJson());
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        Address address = gson.fromJson(connection.getJson(), Address.class);
+        System.out.println(address);
     }
 }
