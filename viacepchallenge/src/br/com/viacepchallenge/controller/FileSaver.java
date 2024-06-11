b022db9ccabc1f946eaef22c86d7e23e1f8ce955
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-11 20:25:32 -0300

    add FileSaver class

---
+++ viacepchallenge/src/br/com/viacepchallenge/controller/FileSaver.java
@@ -0,0 +1,18 @@
+package br.com.viacepchallenge.controller;
+
+import br.com.viacepchallenge.model.Address;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileSaver {
+
+    private static final String fileName = "queries.txt";
+    private static final String filePath = "src/resources/" + fileName;
+
+    public static void saveFile(Address address) throws IOException {
+        FileWriter writer = new FileWriter(filePath);
+        writer.write(address.toString());
+        writer.close();
+    }
+}
