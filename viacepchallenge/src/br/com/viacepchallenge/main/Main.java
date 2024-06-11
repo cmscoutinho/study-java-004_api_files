cabab246b8e1b70590ef304d9f443e9fde396fc6
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-11 13:01:40 -0300

    use a Menu object inside a loop

--- viacepchallenge/src/br/com/viacepchallenge/main/Main.java
+++ viacepchallenge/src/br/com/viacepchallenge/main/Main.java
@@ -2,6 +2,7 @@ package br.com.viacepchallenge.main;
 
 import br.com.viacepchallenge.controller.HttpConnection;
 import br.com.viacepchallenge.model.Address;
+import br.com.viacepchallenge.view.Menu;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -10,14 +11,21 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
-        String query = "67013710";
+        String query;
 
-        HttpConnection connection = new HttpConnection(query);
+        Menu menu = new Menu();
+        menu.welcome();
 
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
-        Address address = gson.fromJson(connection.getJson(), Address.class);
-        System.out.println(address);
+        do {
+            query = menu.read();
+            if (query != null) {
+                HttpConnection connection = new HttpConnection(query);
+                Gson gson = new GsonBuilder()
+                        .setPrettyPrinting()
+                        .create();
+                Address address = gson.fromJson(connection.getJson(), Address.class);
+                System.out.println(address);
+            }
+        } while (query != null);
     }
 }
