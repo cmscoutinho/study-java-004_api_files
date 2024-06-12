85685d8a28447f400ad8cc621a7159cc8f0a8af8
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-06-12 20:22:40 -0300

    configure queryField placeholder

--- screenmatch/src/ex_03_10/ex_03/view/MainWindow.java
+++ screenmatch/src/ex_03_10/ex_03/view/MainWindow.java
@@ -5,6 +5,8 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class MainWindow extends JFrame {
 
@@ -15,15 +17,42 @@ public class MainWindow extends JFrame {
     private JPanel avatarPanel;
     private JPanel infoPanel;
 
+    private JTextField queryField;
+
+    private JButton queryButton;
+
     public MainWindow() {
         this.setTitle(TITLE);
-//        this.setLayout(new BorderLayout(10, 10));
 
+        // Components initialization
         mainPanel = new JPanel();
         queryPanel = new JPanel();
         avatarPanel = new JPanel();
         infoPanel = new JPanel();
 
+        queryField = new JTextField("Github username", 20);
+
+        queryButton = new JButton("Search");
+
+        // Components configuration
+        queryField.setForeground(Color.lightGray);
+        queryField.setToolTipText("Github username");
+        queryField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                queryField.setText("");
+                queryField.setForeground(Color.black);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                queryField.setText("Github username");
+                queryField.setForeground(Color.lightGray);
+            }
+        });
+
+
+        // Panel configuration
         mainPanel.setLayout(new BorderLayout(10, 10));
         mainPanel.setBorder(new EmptyBorder(10,10,10,10));
 
@@ -31,6 +60,7 @@ public class MainWindow extends JFrame {
         avatarPanel.setPreferredSize(new Dimension(460, 460));
         infoPanel.setPreferredSize(new Dimension(460 / 2, 460));
 
+        // Border configuration
         Border border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
         queryPanel.setBorder(border);
         avatarPanel.setBorder(border);
@@ -40,14 +70,19 @@ public class MainWindow extends JFrame {
         mainPanel.add(avatarPanel, BorderLayout.WEST);
         mainPanel.add(infoPanel, BorderLayout.EAST);
 
+        // Components inclusion
+        queryPanel.add(queryField);
+        queryPanel.add(queryButton);
+
         this.add(mainPanel);
 
+        // Frame final settings
         this.pack();
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-
+        this.requestFocus();
     }
 
     public static void main(String[] args) {
